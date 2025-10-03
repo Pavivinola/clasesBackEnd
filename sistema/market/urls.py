005134653d1a8da1b3382bd5f79a162ad1f6bc57
Pavivinola <jorge.moreno33@inacapmail.cl>
2025-10-02 22:27:34 -0300

    Agregado el botón prestar, la vista y la lógica

--- sistema/market/urls.py
+++ sistema/market/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
-urlpatterns = [
+
+urlpatterns = [ #esto sirve para definir las rutas de la app, se pasa de una vista a otra
     path('', views.inicio, name = 'inicio'),
     path('nosotros', views.nosotros, name = 'nosotros'),
     path('inicio', views.inicio, name = 'inicio'),
     path('market', views.market, name= 'market'),
     path('market/crear', views.crear, name='crear'),
-    path('eliminar/<int:producto_id>/', views.eliminar, name='eliminar')
+    path('eliminar/<int:producto_id>/', views.eliminar, name='eliminar'),
+    path('prestar/<int:producto_id>/', views.prestar, name='prestar')
     
 ]
--- sistema/market/views.py
+++ sistema/market/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import  HttpResponse
 from .models import Producto
 from .forms import ProductoForm
+from django.contrib import messages #Este módulo es para enviar mensajes flash a los usuarios
 # Create your views here.
 # def inicio(request):
 #     return HttpResponse("<h1>Holaaa</h1>")
@@ -27,6 +28,18 @@ def crear(request):
         formulario.save()
         return redirect("market") 
     return render(request, "market/crear.html", {'formulario':formulario})
+
+def prestar(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    if producto.stock > 0:
+        producto.stock -= 1
+        producto.save()
+        messages.success(request, f"Se prestó un ejemplar de {producto.titulo}, quedan {producto.stock} disponibles.")
+    else:
+        messages.error(request, f"No hay ejemplares disponibles para el préstamo de este título: {producto.titulo}.")
+    return redirect('market')
+   
+
 
 
 
